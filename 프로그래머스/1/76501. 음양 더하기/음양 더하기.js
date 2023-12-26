449a542c361ca39ef599099c6d51753652556982
멜리 <154109930+imellieyou@users.noreply.github.com>
2023-12-26 14:09:39 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -2,12 +2,18 @@ function solution(absolutes, signs) {
     let answer = 0;
  
     for (let i = 0; i < absolutes.length; i++) {
-        if(signs[i]) {
-            answer += absolutes[i]
-        } else {
-            answer += absolutes[i] * -1
-        }
+      signs[i] ? answer += absolutes[i] : answer -= absolutes[i]
     }
+    
+    // for (let i = 0; i < absolutes.length; i++) {
+    //     if(signs[i]) {
+    //         answer += absolutes[i]
+    //     } else {
+    //         answer += absolutes[i] * -1
+    //     }
+    // }
+    
+    
     
     return answer
 }
