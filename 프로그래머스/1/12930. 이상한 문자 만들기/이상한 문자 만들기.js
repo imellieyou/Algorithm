22fd7e04fde2a1fd4757c20bb04c8831491751eb
멜리 <154109930+imellieyou@users.noreply.github.com>
2023-12-27 10:44:43 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    let answer = []
+    
+    const words = s.split(' ').map(el => [...el]);
+    
+    for(let i = 0; i < words.length; i++) {
+        let keyword = []
+        for (let k = 0; k < words[i].length; k++) {
+            keyword.push(k % 2 ? words[i][k].toLowerCase() : words[i][k].toUpperCase());
+        }
+        answer.push(keyword.join(''))
+    }
+    
+    return answer.join(' ')
+    
+}
