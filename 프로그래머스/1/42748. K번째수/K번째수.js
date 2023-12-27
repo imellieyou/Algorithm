6757dee7a370a4b9a3374c5caece7cde1194eff6
멜리 <154109930+imellieyou@users.noreply.github.com>
2023-12-27 16:54:16 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -2,8 +2,17 @@ function solution(array, commands) {
     let answer = [];
     
     commands.map((arr) => {
-        answer.push(array.slice(arr[0] - 1, arr[1]).sort((a, b) => a - b )[arr[2] - 1]);
+        answer.push(array.slice(arr[0] - 1, arr[1]).sort((a, b) => a - b)[arr[2] - 1]);
     });
 
     return answer;    
 }
+
+
+/*
+입력값 〉 array [10, 2], commands [[1, 2, 1]]
+기댓값 〉 [2]
+
+2번에서 실패해서 체크. 단순 정렬에서 '숫자'기준 정렬로 변경하여 적용  
+*/
+
