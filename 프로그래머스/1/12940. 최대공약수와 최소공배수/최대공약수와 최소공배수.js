880f5fc385172aaed495c220e94aa0c793ebbcb0
멜리 <154109930+imellieyou@users.noreply.github.com>
2024-01-01 22:22:53 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -2,6 +2,7 @@ function solution(n, m) {
     let gcd;
     
     for(let i = 1; i <= Math.min(n,m); i++) {
+        // 최대 공약수 찾기 : n, m 중 작은 값이 나눠져야 하는 값이므로 범위는 작은 값까지
         if (Math.max(n,m) % i === 0 && Math.min(n,m) % i === 0)
             gcd = i;
     };
