b52af868b06810862a864b02fc9af340ed68f569
멜리 <154109930+imellieyou@users.noreply.github.com>
2024-01-02 13:41:43 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.47 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -3,11 +3,9 @@ function solution(numbers) {
     
     for(let i = 0; i < numbers.length; i++) {
         for (let k = i+1; k < numbers.length; k++) {
-            if(!answer.includes(numbers[i] + numbers[k])){
-                answer.push(numbers[i] + numbers[k]);
-            }
+            answer.push(numbers[i] + numbers[k]);
         }
     }
     
-    return answer.sort((a, b) => a - b);
+    return [...new Set(answer)].sort((a, b) => a - b);
 }
