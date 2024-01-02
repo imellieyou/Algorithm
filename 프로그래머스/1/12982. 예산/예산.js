1ff0b8dcf9ed33b3b231890ed7222ca57640d5c4
멜리 <154109930+imellieyou@users.noreply.github.com>
2024-01-02 16:24:38 +0900

    [level 1] Title: 예산, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,14 @@
+function solution(d, budget) {
+    d.sort((a, b) => a - b);
+    
+    let cnt = 0;
+    
+    for (let i = 0; i < d.length; i++) {
+        if(budget - d[i] < 0)  break;
+        
+        budget -= d[i];
+        cnt++;
+    }
+    
+    return cnt;
+}
