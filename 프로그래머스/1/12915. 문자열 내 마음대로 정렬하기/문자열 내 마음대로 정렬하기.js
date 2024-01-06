a7085b6e7f76fba1ca4c973da03ec7b8bbe0a53e
멜리 <154109930+imellieyou@users.noreply.github.com>
2024-01-06 23:32:07 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -4,10 +4,10 @@ function solution(strings, n) {
            // a[n]이 알파벳 상 우선일 때 1을 반환하여 sort에 변화없게 정렬
            return a[n] > b[n] ? 1 : -1;
        }
-       
+
        // 사전상 문자열 정렬
        return a > b ? 1 : -1;
    });
-   
+
    return answer;
 }
