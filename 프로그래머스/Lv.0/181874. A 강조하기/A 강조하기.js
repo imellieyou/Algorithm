9ea9c20f10dedf262946bc4667a65626b02aefbc
멜리 <154109930+imellieyou@users.noreply.github.com>
2024-01-13 10:33:55 +0900

    [level Lv.0] Title: A 강조하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/Lv.0/181874. A 강조하기/A 강조하기.js
@@ -1,13 +1,4 @@
 function solution(myString) {
-    let answer = '';
-    
-    let lowerStr = myString.toLowerCase();
-    
-    for(let i = 0; i < lowerStr.length; i++) {
-        lowerStr[i] === "a"
-        ? answer += lowerStr[i].toUpperCase() 
-        : answer += lowerStr[i];
-    }
-    
-    return answer;
+    // 내가 왜 replaceAll을 까먹었지..?
+    return myString.toLowerCase().replaceAll('a', 'A');
 }
