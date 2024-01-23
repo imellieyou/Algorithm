785dcf058158da195dad2e3e2aa7cb5215469de3
멜리 <154109930+imellieyou@users.noreply.github.com>
2024-01-23 22:46:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,7 +1,7 @@
 function solution(s) {
     const words = s.toLowerCase().split(' ');
-    
-    const result = words.map((word, i) => word && (word[0].toUpperCase() + word.slice(1)));
+    // 문장 처음 공백문자가 연속해서 나왔을 경우에는 word[0]이 공백이 될 수 있어서 word 체크 조건 추가
+    const result = words.map((word, i) => word && word[0].toUpperCase() + word.slice(1));
     
     return result.join(' ');
 }
