fc1f2e1ba24ec8709ac1a39e6193c51327aab196
멜리 <154109930+imellieyou@users.noreply.github.com>
2024-01-23 22:23:59 +0900

    [level 2] Title: 올바른 괄호, Time: 3.06 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,22 +1,18 @@
 function solution(s){
-    if(s.indexOf('(') > 0) return false;
+    // s의 시작이 ')'인 경우 false 리턴
+    if(s.indexOf(')') === 0) return false;
     
-    else {
-        let open = 0;
-        let close = 0
-        
-        for(let i = 0; i < s.length; i++) {            
-            if(s[i] === '(') {
-                open++;
-            } else if(s[i] === ')') {
-                close++
-                if(close > open) return false
-            }
-            
-            // if(s[1] === ')' && s[2] === ')') return false;
+    // s의 시작이 '('인 경우
+    let cnt = 0;
 
-        }
-        
-        return open === close ? true : false;
-    }
+    for(let i = 0; i < s.length; i++) {            
+        if(s[i] === '(') cnt++;
+        else if(s[i] === ')') {
+            cnt--;
+            // ')'가 지금까지의 '('보다 더 많아서 음수가 되면 짝이 맞지 않았다는 뜻이므로 false 리턴 
+            if(cnt < 0) return false;
+        }  
+    } 
+
+    return cnt === 0;
 }
