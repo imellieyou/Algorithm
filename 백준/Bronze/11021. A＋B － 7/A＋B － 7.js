8444a96f4aa96cb6e2e2ff4fbc9b1edb1356f6d7
멜리 <154109930+imellieyou@users.noreply.github.com>
2024-02-07 00:28:26 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 9504 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+const cnt = parseInt(input[0]);
+
+for(let i = 1; i <= cnt; i++) {
+  const A = parseInt(input[i].split(' ')[0]);
+  const B = parseInt(input[i].split(' ')[1]);
+
+  console.log(`Case #${i}: ${A + B}`);
+}
