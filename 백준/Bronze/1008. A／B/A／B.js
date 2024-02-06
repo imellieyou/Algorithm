4967dc4fed41b5fff1ebb2c86ce00d9435e8a70d
멜리 <154109930+imellieyou@users.noreply.github.com>
2024-02-06 23:09:54 +0900

    [Bronze V] Title: A/B, Time: 148 ms, Memory: 9712 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,13 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function(){
+	console.log(parseInt(input[0]) / parseInt(input[1]));
+    process.exit();
+});
