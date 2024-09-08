680f2240c82abd3d7cc9dffa096186a9f023d19b
멜리 <154109930+imellieyou@users.noreply.github.com>
2024-09-08 16:47:26 +0900

    [level 1] Title: 모의고사, Time: 1.53 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,35 +1,18 @@
 function solution(answers) {
-  // 각 수포자의 찍기 패턴 정의
-    const patterns = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ];
-
-    // 각 수포자별 정답 수를 저장할 배열 초기화
-    const score = [0, 0, 0];
-
-    // 주어진 답안을 순회하며 각 수포자의 정답 수 계산
-    for (let i = 0; i < answers.length; i++) {
-        patterns.forEach((pattern, index) => {
-            // i % pattern.length!
-            if (pattern[i % pattern.length] === answers[i]) {
-                // 배열의 특정 인덱스 ++하는 방법!
-                score[index]++;
-            }
-        });
-    }
-
-    // 가장 많은 문제를 맞힌 사람 찾기
+    let score = [0, 0, 0];
+    
+    const student1 = [1, 2, 3, 4, 5];
+    const student2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    
+    answers.forEach((answer, i) => {
+        if (answer === student1[i % student1.length]) score[0]++;
+        if (answer === student2[i % student2.length]) score[1]++;
+        if (answer === student3[i % student3.length]) score[2]++;
+    });
     const maxScore = Math.max(...score);
-    const result = [];
-
-    for (let i = 0; i < score.length; i++) {
-        if (score[i] === maxScore) {
-            result.push(i + 1);
-        }
-    }
-
-    // 오름차순 정렬하여 반환
-    return result.sort();
+    
+    return score
+        .map((s, idx) => (s === maxScore ? idx + 1 : null))
+        .filter(num => num !== null); 
 }
